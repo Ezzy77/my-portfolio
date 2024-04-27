a979b1c1fb7fcaaa8ed5f5f8ade4028274180d9f
Elisio <racabral11@icloud.com>
2024-04-27 10:46:03 +0100

    Add eslint-disable for unescaped entities in page.js
    Two lines in the file app/page.js were modified to ignore eslint warnings about unescaped entities. These changes were made in the div text content that are part of the application's main page.

--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ export default function Home() {
         <main className="page">
             <section className="flex items-center justify-between space-x-4 py-10 px-6 bg-gray-100">
                 <div className="flex-1">
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
                     <h1 className="text-3xl font-bold mb-2">Hello, I'm Elisio Cabral Sa</h1>
                     <h3 className="text-xl mb-4">
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
                         I'm a software engineer specialized in backend web services
                         and CLI tools
                     </h3>
